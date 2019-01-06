eb10f4fa778be9e72cc75c7d3a31721ba9f035fe
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-06 01:10:16 +0100

    Modify package.json

--- js/script.js
+++ js/script.js
@@ -6,7 +6,22 @@
     var flkty = new Flickity( elem, {
     // options
     cellAlign: 'left',
-    contain: true
+    contain: true,
+    hash: true,
+    pageDots: false
+    });
+
+    // scroll
+    var progressBar = document.querySelector('.progress-bar');
+
+    flkty.on('scroll', function (progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        progressBar.style.width = progress * 100 + '%';
+    });
+    // button restart
+    var btnRestart = document.querySelector('.restart-button');
+    btnRestart.addEventListener('click', function () {
+        flkty.select(0);
     });
 
 })();

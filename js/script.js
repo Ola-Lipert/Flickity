796c576ab09dd039f62223c405698c8f50307912
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-06 17:30:43 +0100

    Add button restart and scroll

--- js/script.js
+++ js/script.js
@@ -17,5 +17,15 @@
         flkty.select(0);
     });
 
+    // scroll
+    var flkty = new Flickity('.carousel');
+    var progressBar = document.querySelector('.progress-bar')
+
+    flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+    });
+
+
     
 
